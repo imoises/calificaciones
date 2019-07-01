c08220a50d184e5117eb370e887576a26e84b16b
moises <dotty@live.com.ar>
2019-07-01 04:30:31 -0300

    responder pregunta ok

--- calificaciones/Controllers/AlumnoController.cs
+++ calificaciones/Controllers/AlumnoController.cs
@@ -15,6 +15,8 @@ namespace calificaciones.Controllers
 
         PreguntaService preguntaService = new PreguntaService();
 
+        RespuestaService respuestaServide = new RespuestaService();
+
         // GET: Alumno
         public ActionResult Inicio()
         {
@@ -57,8 +59,30 @@ namespace calificaciones.Controllers
         [HttpPost]
         public ActionResult Responder(int IdPregunta)
         {
-            return View();
+            Pregunta pregunta = preguntaService.ObtenerUnaPreguntaId(IdPregunta);
+
+            return View(pregunta);
         }
+
+        [HttpPost]
+        public ActionResult VerificarRespuesta(int IdPregunta, String respuesta)
+        {
+            Pregunta pregunta = preguntaService.ObtenerUnaPreguntaId(IdPregunta);
+
+            if (pregunta.FechaDisponibleHasta.Value >= DateTime.Now)
+            {
+                respuestaServide.AgregarRespuesta(IdPregunta, respuesta, Convert.ToInt32(Session["Id"]));
+                return Redirect("~/Alumno/Preguntas");
+            }
+            else
+            {
+                TempData["Respuesta"] = respuesta;
+                TempData["Mensaje"] = "Ya paso la fecha de entrega";
+
+                return View("Responder", pregunta);
+            }
+        }
+
     }
 }
         
--- calificaciones/Services/RespuestaService.cs
+++ calificaciones/Services/RespuestaService.cs
@@ -15,5 +15,20 @@ namespace calificaciones.Services
             return bdContexto.RespuestaAlumnoes.Include("Alumno")
                 .Include("ResultadoEvaluacion").Where(r => r.Pregunta.Nro == Id).ToList();
         }
+
+        public void AgregarRespuesta(int idPregunta, String respuesta, int idAlumno)
+        {
+            RespuestaAlumno respuestaAlumno = new RespuestaAlumno();
+
+            respuestaAlumno.IdPregunta = idPregunta;
+            respuestaAlumno.IdAlumno = idAlumno;
+            respuestaAlumno.FechaHoraRespuesta = DateTime.Now;
+            respuestaAlumno.Respuesta = respuesta;
+            respuestaAlumno.Orden = bdContexto.RespuestaAlumnoes.Where(r => r.IdPregunta == idPregunta).ToList().Count + 1;
+
+            bdContexto.RespuestaAlumnoes.Add(respuestaAlumno);
+
+            bdContexto.SaveChanges();
+        }
     }
 }
