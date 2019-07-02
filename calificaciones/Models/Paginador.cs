0ab98ac7d2c584584288e26d6ffb43a6a1770fc0
ferezecarr <ferezecarr@gmail.com>
2019-07-02 09:51:59 -0300

    Subiendo clase paginador

---
+++ calificaciones/Models/Paginador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace calificaciones.Models
+{
+    public class Paginador
+    {
+        public int PaginaActual { get; set; }
+        public int RegistrosPorPagina { get; set; }
+        public int TotalRegistros { get; set; }
+        public int TotalPaginas { get; set; }
+        public IEnumerable<T> Resultado { get; set; }
+    }
+}
