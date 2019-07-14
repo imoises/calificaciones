c1b3e2bf2b8056aa5c25e21b5d5448ab2642e421
Fernando Carreño <ferezecarr@gmail.com>
2019-07-14 14:48:45 -0300

    Actualizando foto en acerca-de y agregando una nueva capa de session

---
+++ calificaciones/Session/SessionManagement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace calificaciones.Session
+{
+    public static class SessionManagement
+    {
+        public static int IdUsuario
+        {
+            get
+            {
+                if(HttpContext.Current.Session["Id"] == null)
+                {
+                    return 0;
+                }
+                return (int)HttpContext.Current.Session["Id"];
+            }
+            set
+            {
+                HttpContext.Current.Session["Id"] = value;
+            }
+        }
+        
+        public static string Roles { get; set; }
+    }
+}
